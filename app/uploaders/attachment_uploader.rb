8b710afb2e3123c4b80b9a2b0f0ce7fa10d6c7da
David Aaron Fendley <tricon@me.com>
2013-01-31 18:42:11 -0600

    Partition uploads.

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,3 +1,8 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
+  include CarrierWave::Compatibility::Paperclip
   storage :file
+
+  def store_dir
+    'system/:class/:attachment/:id_partition'
+  end
 end
