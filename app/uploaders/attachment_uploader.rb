17bf95c5faf22c8a0c6bf732b2ac3bfd487446e3
David Aaron Fendley <tricon@me.com>
2013-03-05 11:35:52 -0600

    Use S3.

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -1,6 +1,6 @@
 class AttachmentUploader < CarrierWave::Uploader::Base
   include CarrierWave::Compatibility::Paperclip
-  storage :file
+  storage :fog
 
   def store_dir
     'system/:class/:attachment/:id_partition'
